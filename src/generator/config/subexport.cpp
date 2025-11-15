cc898505c1f6b98d8196823a8f46d3b401d1d92b
zhongfly <11155705+zhongfly@users.noreply.github.com>
2025-11-15 12:43:04 +0800

    强制 short-id 使用双引号输出

--- src/generator/config/subexport.cpp
+++ src/generator/config/subexport.cpp
@@ -682,6 +682,7 @@ void proxyToClash(std::vector<Proxy> &nodes, YAML::Node &yamlnode, const ProxyGr
             if (!x.PublicKey.empty() && !x.ShortID.empty()) {
                 singleproxy["reality-opts"]["public-key"] = x.PublicKey;
                 singleproxy["reality-opts"]["short-id"] = x.ShortID;
+                singleproxy["reality-opts"]["short-id"].SetStyle(YAML::DoubleQuoted);
                 singleproxy["client-fingerprint"] = "random";
             }
             if (!scv.is_undef())
